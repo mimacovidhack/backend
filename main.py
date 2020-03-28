4612d719d02f029c1dbdbc12e9cc155f6239c928
Arik Sidney Guggenheim <ariksidney@outlook.com>
2020-03-28 11:41:57 +0100

    CI CD Stuff

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, escape, request
+import os
 
 app = Flask(__name__)
+port = int(os.getenv('PORT', 3000))
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=port)
