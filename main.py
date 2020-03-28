616e393dd8588f56fdfebc35f52d3b45ca7b5363
rixlabs <riccardo.causo@mimacom.com>
2020-03-28 13:18:26 +0100

    circle ci working?

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World Gcloud!'
+    return 'Hello World poldo!'
 
 
 @app.errorhandler(500)
@@ -17,4 +17,3 @@ def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 # [END app]
-© 2020 GitHub, Inc.
